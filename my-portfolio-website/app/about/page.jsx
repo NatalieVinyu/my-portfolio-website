398644e7da9fcb02bb37081e0c746109ae85551a
Natalie Vinyu <135601983+NatalieVinyu@users.noreply.github.com>
2024-08-22 14:42:41 +0200

    added my font and made my navbar blurry

--- my-portfolio-website/app/about/page.jsx
+++ my-portfolio-website/app/about/page.jsx
@@ -2,13 +2,12 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHtml5, faCss3Alt, faJs, faReact  } from '@fortawesome/free-brands-svg-icons';
 
-
 const About = () => {
   return (
-    <div id='about' className='container mx-auto'>
+    <div id='about' className='container mx-auto pt-16'>
       <div className='grid grid-cols-2 gap-4'>
-        <p>
-          <span className='text-2xl'>Hello! My I am Natalie Vinyu, and I’m a passionate self-taught front-end developer.</span>
+        <p className='font-sans'>
+          <span className='text-2xl'>Hello! I am Natalie Vinyu, and I’m a passionate self-taught front-end developer.</span>
         <br />
            My journey into web development began out of sheer curiosity, which quickly grew into a full-fledged passion. I’ve spent countless hours honing my skills in HTML, CSS, JavaScript, and React, building everything from simple landing pages to dynamic, responsive web applications. I’m always eager to learn and embrace new challenges, and I thrive in environments where creativity meets technology. I’m excited to bring my self-driven, problem-solving mindset to new projects and collaborate with like-minded individuals to create impactful digital experiences.</p>
       </div>
--- my-portfolio-website/app/home/page.jsx
+++ my-portfolio-website/app/home/page.jsx
@@ -4,9 +4,9 @@ const Homepage = () => {
   return (
     <div className='flex items-center justify-center min-h-screen'>
         <div className='container mx-auto'>
-            <div className=''>
-              <h1 className='text-9xl'>Hi, I'm Natalie Vinyu</h1>
-              <h2 className='text-8xl'>Front-end developer</h2>
+            <div className='font-sans'>
+              <h1 className='text-9xl'>Natalie Vinyu</h1>
+              <h2 className='text-7xl'>Front-end developer</h2>
               
             </div>
         </div>
