258fc23ef08bbbb9cf1aa383567720ec3088c805
Natalie Vinyu <135601983+NatalieVinyu@users.noreply.github.com>
2024-09-27 14:31:22 +0200

    Data mapping

--- my-portfolio-website/app/home/page.jsx
+++ my-portfolio-website/app/home/page.jsx
@@ -5,7 +5,7 @@ const Homepage = () => {
     <div className='flex items-center justify-center min-h-screen'>
         <div className='container mx-auto'>
             <div className='font-mono'>
-              <h1 className='text-9xl hover:animate-slideInLeft'>Natalie Vinyu</h1>
+              <h1 className='text-9xl'>Natalie Vinyu</h1>
               <h2 className='text-7xl'>Front-end developer</h2>
               
             </div>
--- my-portfolio-website/app/mywork/page.jsx
+++ my-portfolio-website/app/mywork/page.jsx
@@ -1,31 +1,19 @@
 import React from 'react'
 import Card from '../card/page';
 
+const cardData = [
+  { title: 'Card Title 1', content: 'This is just an example for the projects i have done', imageUrl: 'coming soon' },
+  { title: 'Card Title 2', content: 'This is just an example for the projects i have done', imageUrl: 'coming soon' }
+]
+
 const Mywork = () => {
   return (
       <div id='mywork' className='container mx-auto min-h-screen '>
         <h1 className='text-4xl font-bold mb-6 pt-16'>What i do</h1>
           <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6'>
-            <Card 
-              title='Card Title 1'
-              content='This is just an example for the projects i have done'
-              imageUrl='coming.soon'
-            />
-            <Card 
-            title='Card Title 2'
-            content='This is just an example for the projects i have done'
-            imageUrl='coming.soon'
-            />
-            <Card 
-            title='Card Title 3'
-            content='This is just an example for the projects i have done'
-            imageUrl='coming.soon'
-            />
-             <Card 
-            title='Card Title 4'
-            content='This is just an example for the projects i have done'
-            imageUrl='coming.soon'
-            />             
+            { cardData.map(({ title, content, imageUrl }, index ) => 
+            ( <Card key={index} title={title} content={content} imageUrl={imageUrl} /> 
+            ))}
           </div>
       </div>
     
